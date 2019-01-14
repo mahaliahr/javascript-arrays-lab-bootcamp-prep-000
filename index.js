dd54eb99998aab8e0ec694dc92a84fe970b7e598
Mahalia Henry-Richards <mahalia_hr@outlook.com>
2019-01-14 21:57:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(){
+
+}
+
+function destructivelyPrependKitten() {
+
+}
+
+function destructivelyRemoveLastKitten() {
+
+}
+
+function destructivelyRemoveFirstKitten() {
+
+}
+
+function appendKitten() {
+
+}
+
+function prependKitten() {
+
+}
+
+function removeLastKitten() {
+
+}
+
+function removeFirstKitten() {
+
+}
